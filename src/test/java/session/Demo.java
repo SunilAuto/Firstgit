747972a09c2fe2ad8a4d93ec31adc6a4d8f2ca49
Admin <Admin@DESKTOP-ERCFOQ7>
2025-06-16 15:02:54 +0530

    New commit

--- src/test/java/session/Demo.java
+++ src/test/java/session/Demo.java
@@ -11,7 +11,7 @@ public class Demo {
 	public static void main(String[] args) throws InterruptedException
 	{
 		// TODO Auto-generated method stub
-
+ 
 		String projectpath=System.getProperty("user.dir");
 		System.out.println("Projectpath:"+projectpath);
 		System.setProperty("webdriver.chrome.driver",projectpath+"/Drivers/ChromeDriver/chromedriver.exe");
@@ -25,7 +25,7 @@ public class Demo {
 		
 		/*driver.get("https://www.facebook.com/reg/");
 		Select selectobject1 = new Select(driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[2]/div[2]/span/span/select[1]")));
-		selectobject1.selectByValue("15");
+		selectobject1.selectByValue("15");594997
 		driver.findElement(By.xpath("/html/body/div[1]/div[1]/div[1]/div[2]/div/div[2]/div/div/div[1]/form/div[1]/div[4]/span/span[1]/label/input")).click();
 		*/
 		Thread.sleep(15000);
---
+++ src/test/java/session/Sample.java
@@ -0,0 +1,10 @@
+package session;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
